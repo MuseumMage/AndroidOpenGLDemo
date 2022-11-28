82b3e3e4004d29cc181ff9f3f31dc35017d664cb
MuseumMage <mageboer@qq.com>
2022-11-28 18:09:26 +0800

    init

---
+++ app/src/main/java/com/example/androidlearnopengl/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.androidlearnopengl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    // 1. create linear layout view with fragment
+    // 2. create navigation button
+    // 3. create opengl surface view
+}
