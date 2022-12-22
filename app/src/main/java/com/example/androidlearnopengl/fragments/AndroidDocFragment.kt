afb36172bdb7a38ac8f346dc93492bae32d82bf4
ZhangBo <mageboer@qq.com>
2022-12-22 23:49:09 +0800

    删除MySurfaceView

--- app/src/main/java/com/example/androidlearnopengl/fragments/AndroidDocFragment.kt
+++ app/src/main/java/com/example/androidlearnopengl/fragments/AndroidDocFragment.kt
@@ -1,22 +1,26 @@
 package com.example.androidlearnopengl.fragments
 
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.androidlearnopengl.MyGLSurfaceView
+import com.example.androidlearnopengl.renderers.AndroidDocRenderer
 
 
 class AndroidDocFragment : Fragment() {
 
-    private lateinit var gLView: MyGLSurfaceView
+    private lateinit var gLView: GLSurfaceView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        gLView = MyGLSurfaceView(requireActivity())
+        gLView = GLSurfaceView(activity).apply {
+            setEGLContextClientVersion(2)
+            setRenderer(AndroidDocRenderer())
+        }
         return gLView
     }
 }
