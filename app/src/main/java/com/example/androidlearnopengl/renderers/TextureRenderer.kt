073fbfdf2b134a5b916e3d97f4ad2e3c51fa34da
ZhangBo <mageboer@qq.com>
2023-01-03 20:43:15 +0800

    修改texture朝向

--- app/src/main/java/com/example/androidlearnopengl/renderers/TextureRenderer.kt
+++ app/src/main/java/com/example/androidlearnopengl/renderers/TextureRenderer.kt
@@ -12,10 +12,10 @@ import javax.microedition.khronos.opengles.GL10
 
 val verticesTexture = floatArrayOf(     // in counterclockwise order:
     // positions          // colors           // texture coords
-    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 0.0f, // top right
+    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 1.0f, // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f, // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f  // top left
 )
 
 val indices = byteArrayOf(
